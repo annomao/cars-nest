4547e4331204dc37b775e933f557519861aa0a88
annomao <omaoannkerubo@gmail.com>
2022-08-02 01:45:21 +0300

    add user signup functionality

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,38 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaUserAlt,FaUserCircle,FaAt,FaLock,FaArrowRight } from 'react-icons/fa'
+import useUser from '../../hooks/useUser'
 import BaseForm from './BaseForm'
 
 function Signup() {
+  const [userSignupData, setUserSignupData] = useState({})
+  const {setAuth} = useUser()
+
   const title = "REGISTER FOR AN ACCOUNT"
   const check = "Already have an account?"
   const link = '/login'
+
+  function handleSubmit(event){
+    event.preventDefault()
+    fetch("",{
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(userSignupData)
+    }
+    )
+    .then(res => res.json())
+    .then(data => setAuth(data))
+
+  }
+
+  function handleChange(event){
+    setUserSignupData({
+      ...userSignupData,
+      [event.target.name]: event.target.value
+    })
+  }
   return ( 
     <>
     <BaseForm title={title} check={check} link={link}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col mb-6">
           <div className="relative">
             <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400"><span><FaUserAlt/></span></div>
-            <input id="fullname" type="text" name="fullname" className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 border-b border-cBlue bg-neutral-100 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="Fullnames..." />
+            <input 
+            onChange={handleChange}
+            id="name" type="text" name="name" className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 border-b border-cBlue bg-neutral-100 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="Fullnames..." 
+            />
           </div>
         </div>
         <div className="flex flex-col mb-6">
           <div className="relative">
             <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400"><span><FaUserCircle/></span></div>
-            <input id="username" type="text" name="username" className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 border-b border-cBlue bg-neutral-100 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="Username..." />
+            <input 
+            onChange={handleChange}
+            id="username" type="text" name="username" className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 border-b border-cBlue bg-neutral-100 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="Username..." 
+            />
           </div>
         </div>
         <div className="flex flex-col mb-6">
           <div className="relative">
             <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400"><span><FaAt/></span></div>
-            <input id="email" type="email" name="email" className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 border-b border-cBlue bg-neutral-100 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="E-Mail Address" />
+            <input 
+            onChange={handleChange}
+            id="email" type="email" name="email" className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 border-b border-cBlue bg-neutral-100 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="E-Mail Address" 
+            />
           </div>
         </div>
         <div className="flex flex-col mb-6">
           <div className="relative">
             <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400"><span><FaLock/></span>
             </div>
-            <input id="password" type="password" name="password" className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 border-b border-cBlue bg-neutral-100 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="Password" />
+            <input 
+            onChange={handleChange}
+            id="password" type="password" name="password" className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 border-b border-cBlue bg-neutral-100 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="Password" 
+            />
           </div>
         </div>
         <div className="flex w-full">

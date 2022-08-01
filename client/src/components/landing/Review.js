4455e2980ab78dd417bc887f7732d4a53b7cc628
annomao <omaoannkerubo@gmail.com>
2022-08-01 06:08:58 +0300

    add reviews page

---
+++ client/src/components/landing/Review.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import LandingTemp from './LandingTemp'
+
+function Review() {
+  const holder = "Search by tag...."
+  const text = "Post a review"
+  return (
+    <>
+    <LandingTemp holder={holder} text={text}/>
+    </>
+  )
+}
+
+export default Review

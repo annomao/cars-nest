ab7230297bf4a880ae0c5743cf0c4cbbf1da14a3
annomao <omaoannkerubo@gmail.com>
2022-08-01 23:48:18 +0300

    add user actions for login and signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def create
+    user = User.create!(allowed_params)
+    render json: user, status: :created
+  end
+
+  def show
+    user = user.find(session[:user_id])
+    render json: user, status: :ok
+  end
+
+  private 
+
+  def allowed_params
+    params.permit(:name, username,email,password)
+  end
+end

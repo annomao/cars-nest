00dfaea3ed7259d6e416ac7b7086013f965525e6
annomao <omaoannkerubo@gmail.com>
2022-08-02 01:37:38 +0300

    add frontend login functionality

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,26 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BaseForm from './BaseForm'
 import { FaAt,FaLock,FaArrowRight } from 'react-icons/fa'
+import useUser from '../../hooks/useUser'
 
 function Login() {
+  const [userData, setUserData] = useState({})
+  const { setAuth } = useUser()
+
   const title = "LOGIN TO YOUR ACCOUNT"
   const check = "Don't have an account?"
   const link = '/signup'
+
+  function handleSubmit(event){
+    event.preventDefault()
+    fetch("",{
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(userData)
+    }
+    )
+    .then(res => res.json())
+    .then(data => setAuth(data))
+
+  }
+
+  function handleChange(event){
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value
+    })
+  }
+
   return ( 
     <>
     <BaseForm title={title} check={check} link={link}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col mb-6">
           <div className="relative">
             <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400"><span><FaAt/></span></div>
-            <input id="email" type="email" name="email" className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 border-b border-cBlue bg-neutral-100 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="E-Mail Address" />
+            <input 
+            id="email" 
+            type="email" 
+            name="email" 
+            className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 border-b border-cBlue bg-neutral-100 w-full py-2 focus:outline-none focus:border-blue-400" 
+            placeholder="E-Mail Address" 
+            onChange={handleChange}
+            />
           </div>
         </div>
         <div className="flex flex-col mb-6">
           <div className="relative">
             <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400"><span><FaLock/></span>
             </div>
-            <input id="password" type="password" name="password" className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 border-b border-cBlue bg-neutral-100 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="Password" />
+            <input 
+            id="password" 
+            type="password" 
+            name="password" 
+            className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 border-b border-cBlue bg-neutral-100 w-full py-2 focus:outline-none focus:border-blue-400" 
+            placeholder="Password" 
+            onChange={handleChange}
+            />
           </div>
         </div>
         <div className="flex w-full">

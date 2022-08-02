dcb1231ece3716844fc7077ea0cc51767664d1b6
annomao <omaoannkerubo@gmail.com>
2022-08-03 02:31:11 +0300

    fix empty array error 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       render json: user, serializer:UserSerializer, status: :created
     else
-      render json: { error: ["Invalid username or password"] }, status: :unauthorized
+      render json: { errors: ["Invalid username or password"] }, status: :unauthorized
     end
   end
 

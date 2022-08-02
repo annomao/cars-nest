e793c15656bf0183a73ee1ca28337a0500376049
annomao <omaoannkerubo@gmail.com>
2022-08-03 01:20:26 +0300

    fix routing error

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       resources :questions
       resources :quecomments, only: [:create, :update, :destroy]
       resources :revcomments, only: [:create, :update, :destroy]
-      get '/me' to: 'users#show' 
+      get '/me', to: 'users#show' 
       post '/auth/login', to: 'sessions#create'
       delete '/auth/logout', to: 'sessions#destroy'
     end

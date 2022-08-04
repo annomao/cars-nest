36e08179cbe39ee0042fc5bb79da75cc28d5e63b
annomao <omaoannkerubo@gmail.com>
2022-08-04 03:33:06 +0300

    change authorizations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  skip_before_action :authorize, only: :create
+  skip_before_action :authorize, only: [:create, :show]
 
   def create
     user = User.create!(allowed_params)

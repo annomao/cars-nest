11b1860abd26444fb66aa279952ce467ed9a1eb4
annomao <omaoannkerubo@gmail.com>
2022-08-04 12:59:54 +0300

    fix error

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -2,7 +2,7 @@ class QuestionSerializer < ActiveModel::Serializer
   attributes :id,:title, :description, :image, :categories, :summary
 
   belongs_to :user
-  has_many :revcomments
+  has_many :quecomments
 
   def summary
     "#{self.object.title} - #{self.object.description[0..49]}..."

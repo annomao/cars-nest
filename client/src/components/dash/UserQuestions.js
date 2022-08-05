6e453c042d3fe08bcef407a706aaa8a98ed3ea65
annomao <omaoannkerubo@gmail.com>
2022-08-05 06:12:40 +0300

    fix error

--- client/src/components/dash/UserQuestions.js
+++ client/src/components/dash/UserQuestions.js
@@ -30,7 +30,7 @@ function UserQuestions() {
       <div className="md:flex no-wrap md:-mx-2 ">
       <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
       {user &&  user.questions.map(question =>{
-        return <PostCard key={question.id} post={question} url={`https://cars-nest.herokuapp.com/api/v1/questions/${review.id}`} onEdit={handleEdit}/>
+        return <PostCard key={question.id} post={question} url={`https://cars-nest.herokuapp.com/api/v1/questions/${question.id}`} onEdit={handleEdit}/>
         })
       }
       </div>

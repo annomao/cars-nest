875ce1709e2e7c403e738bb4f4d0e386e94be985
annomao <omaoannkerubo@gmail.com>
2022-08-05 04:50:10 +0300

    debug

--- client/src/components/dash/UserReviews.js
+++ client/src/components/dash/UserReviews.js
@@ -8,19 +8,19 @@ function UserReviews() {
     fetch("https://cars-nest.herokuapp.com/api/v1/user/posts")
     .then(res => res.json())
     .then(data => setUser(data))
-  })
+  },[])
 
   console.log(user)
 
-  const displayedReviews = user.reviews.map(review =>{
-    return <PostCard post={review}/>
-  })
+  // const displayedReviews = user.reviews.map(review =>{
+  //   return <PostCard post={review}/>
+  // })
 
   return (
     <div className="container mx-auto my-5 p-5">
       <div className="md:flex no-wrap md:-mx-2 ">
       <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-      {displayedReviews}
+      {/* {displayedReviews} */}
       </div>
       </div>
     </div>

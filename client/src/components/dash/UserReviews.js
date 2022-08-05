3c579ef4cd5e63167e8e6b539500400a8c8ce270
annomao <omaoannkerubo@gmail.com>
2022-08-05 05:01:04 +0300

    Add conditional rendering

--- client/src/components/dash/UserReviews.js
+++ client/src/components/dash/UserReviews.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import PostCard from './PostCard'
 
 function UserReviews() {
-  const [user,setUser] = useState({})
+  const [user,setUser] = useState(null)
 
   useEffect(()=>{
     fetch("https://cars-nest.herokuapp.com/api/v1/user/posts")
@@ -12,15 +12,15 @@ function UserReviews() {
 
   console.log(user)
 
-  // const displayedReviews = user.reviews.map(review =>{
-  //   return <PostCard post={review}/>
-  // })
 
   return (
     <div className="container mx-auto my-5 p-5">
       <div className="md:flex no-wrap md:-mx-2 ">
       <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-      {/* {displayedReviews} */}
+      {user &&  user.reviews.map(review =>{
+        return <PostCard post={review}/>
+        })
+      }
       </div>
       </div>
     </div>
